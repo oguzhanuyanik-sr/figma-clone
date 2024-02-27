1d8ff2ddea0b131359ba1641e6c0af5183fe98bd
oguzhanuyanik-sr <oguzhanuyanik.sr@gmail.com>
2024-02-28 00:06:27 +0300

    LiveCursor & CursorChat added

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,10 @@
+'use client';
+import Live from '@/components/Live';
+
 export default function Page() {
-  return <h1 className='text-5xl text-white'>Liveblocks</h1>;
+  return (
+    <div>
+      <Live />
+    </div>
+  );
 }
